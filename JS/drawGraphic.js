805d3f3752a44720d62d63ad76e273a2ad0dda31
“Xuchen1591” <magina.1591@outlook.com>
2015-07-16 23:07:40 +0800

    draw use path

--- JS/drawGraphic.js
+++ JS/drawGraphic.js
@@ -7,6 +7,9 @@ function draw(id){
         return false;
     var context = canvas.getContext('2d');
     context.fillStyle = "#ffeeef";
+
+
+    //rectangle
     context.fillRect(0,0,400,300);
 
     context.fillStyle = 'red';
@@ -16,4 +19,14 @@ function draw(id){
     context.strokeRect(50,50,100,100);
 
     context.clearRect(60,60,80,90);
+
+
+    
+    //use path to draw a graphic
+    //circle
+    context.beginPath();
+    context.arc(400,300,100,0,Math.PI*2,true);
+    context.closePath();
+    context.fillStyle = 'rgba(255,0,0,0.25)';
+    context.fill();
 }
