8eec95feb1a5659b048d62e7548a5951fe531ac3
“Xuchen1591” <magina.1591@outlook.com>
2015-07-18 14:33:03 +0800

    color gradient

--- JS/drawGraphic.js
+++ JS/drawGraphic.js
@@ -89,6 +89,13 @@ function draw(id){
     var g2 = context.createLinearGradient(0,0,300,0);
     g2.addColorStop(0,'rgba(0,0,255,0.5)');
     g2.addColorStop(1,'rgba(255,0,0,0.5)');
+    for(var i=0;i<10;i++){
+        context.beginPath();
+        context.fillStyle = g2;
+        context.arc(i*25,i*25,i*10,0,Math.PI*2,true);
+        context.closePath();
+        context.fill();
+    }
 }
 
 
