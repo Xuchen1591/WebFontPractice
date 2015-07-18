672ed3c4c892ba34fa7df962fb3e00fa30130942
“Xuchen1591” <magina.1591@outlook.com>
2015-07-18 13:09:08 +0800

    moveTo/lineTo

--- JS/drawGraphic.js
+++ JS/drawGraphic.js
@@ -6,9 +6,10 @@ function draw(id){
     if(canvas == null)
         return false;
     var context = canvas.getContext('2d');
-    context.fillStyle = "#ffeeef";
+    //context.fillStyle = "#ffeeef";
 
 
+    /*
     //rectangle
     context.fillRect(0,0,400,300);
 
@@ -21,7 +22,7 @@ function draw(id){
     context.clearRect(60,60,80,90);
 
 
-    
+
     //use path to draw a graphic
     //circle
     context.beginPath();
@@ -29,4 +30,82 @@ function draw(id){
     context.closePath();
     context.fillStyle = 'rgba(255,0,0,0.25)';
     context.fill();
+
+
+
+    //draw lines
+    //moveTo and lineTo
+    var n = 0;
+    var dx = 500;
+    var dy = 600;
+    var s = 100;
+    context.beginPath();
+    context.fillStyle = 'rgb(100,255,100)';
+    context.strokeStyle = 'rgb(0,0,100)';
+    var x = Math.sin(0);
+    var y = Math.cos(0);
+    var dig = Math.PI/15*11;
+    for(var i=0;i<30;i++){
+        var x = Math.sin(i*dig);
+        var y = Math.cos(i*dig);
+        context.lineTo(dx + x*s, dy + y*s);
+    }
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+
+
+    context.beginPath();
+    context.fillStyle = 'rgb(100,255,100)';
+    context.strokeStyle = 'rgb(0,0,100)';
+    //context.moveTo(700,600);
+    context.lineTo(900,600);
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+
+    //test lineTo...
+    //seems some problem
+    context.fillStyle = 'rgb(100,255,100)';
+    context.strokeStyle = 'rgb(0,0,100)';
+    context.lineTo(700,600);
+    context.closePath();
+    context.fill();
+    context.stroke();
+     */
+
+
+    //LinerGradient
+    //context.createLinearGradient(xStart, yStart, xEnd, yEnd)
+    //context.addColorStop(offset, color)
+    var g1 = context.createLinearGradient(0,0,0,300);
+    g1.addColorStop(0,'rgb(255,255,0)');
+    g1.addColorStop(1,'rgb(0,255,255)');
+    context.fillStyle = g1;
+    context.fillRect(0,0,400,300);
+
+    var g2 = context.createLinearGradient(0,0,300,0);
+    g2.addColorStop(0,'rgba(0,0,255,0.5)');
+    g2.addColorStop(1,'rgba(255,0,0,0.5)');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
